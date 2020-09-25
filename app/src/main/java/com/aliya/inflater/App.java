091cb0185398135c6d34f80a9a5fcc17aacd6970
a_liYa <a_liYa@126.com>
2020-09-25 16:12:07 +0800

    添加耗时对比

--- app/src/main/java/com/aliya/inflater/App.java
+++ app/src/main/java/com/aliya/inflater/App.java
@@ -2,16 +2,16 @@ package com.aliya.inflater;
 
 import android.app.Application;
 
-import com.aliya.api.ViewCreatorAnnotation;
-
 /**
  * App
  *
  * @author a_liYa
  * @date 2020/9/23 16:14.
  */
-@ViewCreatorAnnotation
 public class App extends Application {
 
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
--- app/src/main/java/com/aliya/inflater/MainActivity.java
+++ app/src/main/java/com/aliya/inflater/MainActivity.java
@@ -1,14 +1,62 @@
 package com.aliya.inflater;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.aliya.view.ViewCreatorImpl;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewCreatorImpl mViewCreator;
+    private ViewInflater mViewInflater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mViewCreator = new ViewCreatorImpl();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    long reflectTakeTime;
+    long newTakeTime;
+    int runCount;
+
+    @Nullable
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        View view = super.onCreateView(name, context, attrs);
+        if (view == null) {
+            long uptimeMillis = SystemClock.uptimeMillis();
+            view = mViewCreator.createView(name, context, attrs);
+            newTakeTime += (SystemClock.uptimeMillis() - uptimeMillis);
+
+//            uptimeMillis = SystemClock.uptimeMillis();
+//            view = ViewInflater.createViewFromTag(context, name, attrs);
+//            reflectTakeTime += (SystemClock.uptimeMillis() - uptimeMillis);
+
+            runCount++;
+        }
+        return view;
+    }
+
+    public void onClick(View view) {
+        LayoutInflater layoutInflater = getLayoutInflater();
+        for (int i = 0; i < 10000; i++) {
+            View inflate = layoutInflater
+                    .inflate(R.layout.layout_single_view, (ViewGroup) view.getParent(), false);
+        }
+        Log.e("TAG", "创建次数: " + runCount + ", 相差时长：" + (reflectTakeTime - newTakeTime));
+        Log.e("TAG", "反射耗时: " + reflectTakeTime);
+        Log.e("TAG", "New耗时: " + newTakeTime);
+
     }
 }
